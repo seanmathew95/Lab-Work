30df38b903a0b88e818a2522d6dc1b473c6ae6ab
Sean Mathew <naeswehtam@g.ucla.edu>
2017-03-12 22:10:40 -0700

    final commit

---
+++ exercise-9/shell-script-demo/process_canid_data.R
@@ -0,0 +1,19 @@
+source("~/PyRate/pyrate_utilities.r")
+
+# we need to give the utilities a list of extant species
+extant_dogs = c("Canis rufus","Canis lupus","Canis aureus","Canis latrans","Canis mesomelas","Canis anthus","Pseudalopex gymnocercus","Canis adustus","Canis familiaris")
+
+# use the extract.ages.pbdb() function in pyrate_utilities to reformat our dataset...
+extract.ages.pbdb(file= "canid_occ.csv",extant_species=extant_dogs)
+
+# you should see the following output:
+
+# "This function is currently being tested - caution with the results!"
+# replicate 1
+# PyRate input file was saved in:  ./canid_occ_PyRate.py 
+
+
+# TO DO:
+# save the file as "process_canid_data.R" in exercise-9
+
+# quit rstudio
